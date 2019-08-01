78c2fc9b9b4d7968e03e1b310d88806fac8ca0bc
Chris Klosowski <cklosowski@gmail.com>
2019-08-01 16:29:12 -0700

    A nicer, but still humor driven restart message #1726

--- plugins/lando-core/tasks/restart.js
+++ plugins/lando-core/tasks/restart.js
@@ -14,7 +14,7 @@ module.exports = lando => {
       const app = lando.getApp(options._app.root);
       // Restart it if we can!
       if (app) {
-        console.log(chalk.green('Stopping your app... just so we can start it up again ¯\\_(ツ)_/¯'));
+        console.log(chalk.green('Stopping your app. Your app will restart after these messages...'));
         return utils.appToggle(app, 'restart', table, lando.cli.makeArt());
       }
     },
